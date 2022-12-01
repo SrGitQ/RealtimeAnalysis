da02f3acd8ccce79c655c9d5ca06ddb8919b86f8
JMBalanzar <1909011@upy.edu.mx>
2022-12-01 15:17:21 -0600

    tuple information

--- hashtag-decoder/src/Components/GeneralInformation.tsx
+++ hashtag-decoder/src/Components/GeneralInformation.tsx
@@ -10,32 +10,32 @@ type Props = {
 
 const GeneralInformation: React.FC <Props> = ({ data }) => {
 	return (
-		<div className="h-2/3 flex ">
+		<div className="h-[28rem] flex">
 			<div className="w-1/5 flex flex-col space-y-8">
-				<Bullet title="Tweets" text={`${data.tweets_count}`}/>
-				<Bullet title="Users" text={`${data.users_count}`}/>
-				<Bullet title="Mentions" text={`${data.mentions}`}/>
+				<Bullet title="Tweets" text={`${data?.one?.tweets_count} | ${data?.two?.tweets_count}`}/>
+				<Bullet title="Users" text={`${data?.one?.users_count} | ${data?.two?.users_count}`}/>
+				<Bullet title="Mentions" text={`${data?.one?.mentions} | ${data?.two?.mentions}`}/>
 			</div>
 			<div className="w-2/3">
-				<GlobeRender marks={data.locations}/>
+				<GlobeRender marks={data}/>
 			</div>
 			<div className="w-1/5 flex flex-col space-y-8">
-				<Bullet title="Places" text={`${data.locations_count}`}/>
+				<Bullet title="Places" text={`${data?.one?.locations_count} | ${data?.two?.locations_count}`}/>
 				<div className="w-18 flex flex-col m-auto">
 					<div className="text-white flex justify-between w-[4rem]">
 						<FontAwesomeIcon icon={solid('laptop')} />
-						<div>{data?.devices?.web}</div>
+						<div>{data?.one?.devices?.web} | {data?.two?.devices?.web}</div>
 					</div>
 					<div className="text-white flex justify-between w-[4rem]">
 						<FontAwesomeIcon icon={solid('apple-whole')} />
-						<div>{data?.devices?.ios}</div>
+						<div>{data?.one?.devices?.ios} | {data?.two?.devices?.ios}</div>
 					</div>
 					<div className="text-white flex justify-between w-[4rem]">
 						<FontAwesomeIcon icon={solid('mobile')} />
-						<div>{data?.devices?.android}</div>
+						<div>{data?.one?.devices?.android} | {data?.two?.devices?.android}</div>
 					</div>
 				</div>
-				<Bullet title="Verified Users" text={`${data.verified_count}`}/>
+				<Bullet title="Verified Users" text={`${data?.one?.verified_count} | ${data?.two?.verified_count}`}/>
 			</div>
 		</div>
 	);
